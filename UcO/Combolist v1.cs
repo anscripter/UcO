0f4d118e197d8f464a60e830cfc966dfb406fb41
Anon Scripter <46980958+anscripter@users.noreply.github.com>
2019-03-03 21:14:45 -0500

    Add files via upload

---
+++ UcO/Combolist v1.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UcO
+{
+    public partial class Combolist_v1 : Telerik.WinControls.UI.RadForm
+    {
+        public Combolist_v1()
+        {
+            InitializeComponent();
+        }
+
+        string[] accs = null;
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            st:
+            if (accs != null)
+            {
+                try
+                {
+                    new Convert(accs).ShowDialog();
+                }
+                catch (Exception ex)
+                {
+                    var mes = MessageBox.Show(ex.Message, "UcO Discovered an error!", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    if (mes == DialogResult.Retry)
+                    {
+                        goto st;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No combolist detected.", "UcO");
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        { 
+            if (accs != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "UcO|*.txt";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, string.Join("\r\n", accs));
+                }
+            }
+            else
+            {
+                MessageBox.Show("No combolist detected.", "UcO");
+            }
+        }
+
+        private void nulled_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "cmd",
+                Arguments = "/C start https://www.nulled.to/!2189148",
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+            p.Start();
+        }
+
+        private void shuffle_Click(object sender, EventArgs e)
+        {
+            if (accs != null)
+            {
+                Control.CheckForIllegalCrossThreadCalls = false;
+                Task.Run(() =>
+                {
+                    DisableAll();
+                    shuffle.Text = "Shuffling...";
+                    Randomize(accs);
+                    shuffle.Text = "Shuffed!";
+                    EnableAll();
+                });
+            }
+            else
+            {
+                MessageBox.Show("No combolist detected.", "UcO");
+            }
+        }
+
+        Random ran = new Random();
+        private void Randomize<T>(T[] items)
+        {
+            for (int i = 0; i < items.Length - 1; i++)
+            {
+                int j = ran.Next(i, items.Length);
+                T temp = items[i];
+                items[i] = items[j];
+                items[j] = temp;
+            }
+        }
+
+        private void EnableAll()
+        {
+            browse.Enabled = true;
+            shuffle.Enabled = true;
+            convert.Enabled = true;
+            save.Enabled = true;
+        }
+
+        private void DisableAll()
+        {
+            browse.Enabled = false;
+            shuffle.Enabled = false;
+            convert.Enabled = false;
+            save.Enabled = false;
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            DisableAll();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "UcO|*.txt";
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Task.Run(() =>
+                {
+                    string[] parsed = File.ReadAllLines(ofd.FileName).Where(a => a.Contains(":")).ToArray();
+                    browse.Text = "Scanning...";
+                    location.Text = ofd.FileName;
+                    accs = parsed;
+                    browse.Text = "Success!";
+                    scanned.Text = "Accounts Scanned: " + parsed.Count();
+                    EnableAll();
+                });
+            }
+            else
+            {
+                EnableAll();
+            }
+        }
+    }
+}
---
+++ UcO/Convert.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace UcO
+{
+    public partial class Convert : Telerik.WinControls.UI.RadForm
+    {
+        public Convert(string[] data)
+        {
+            InitializeComponent();
+            d = data;
+        }
+
+        Random ran = new Random();
+        string[] d = null;
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            List<string> filter = new List<string>(d);
+            if (e_to_u.IsChecked)
+            {
+                List<string> accounts = new List<string>();
+
+                foreach(string acc in filter.ToArray())
+                {
+                    try
+                    {
+                        string a = acc.Split('@')[0];
+                        string p = acc.Split(':')[1];
+                        accounts.Add(a + ":" + p);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "UcO|*.txt";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, string.Join("\r\n", accounts));
+                    MessageBox.Show("Success!", "UcO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            if (u_to_e.IsChecked)
+            {
+                List<string> accounts = new List<string>();
+
+                foreach (string acc in filter.ToArray())
+                {
+                    try
+                    {
+                        string a = acc.Split(':')[0];
+                        string p = acc.Split(':')[1];
+
+                        switch (ran.Next(0, 3))
+                        {
+                            case 0:
+                                {
+                                    a = a + "@gmail.com";
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    a = a + "@hotmail.com";
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    a = a + "@outlook.com";
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    a = a + "@yahoo.com";
+                                    break;
+                                }
+                        }
+
+
+                        accounts.Add(a + ":" + p);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "UcO|*.txt";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, string.Join("\r\n", accounts));
+                    MessageBox.Show("Success!", "UcO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
